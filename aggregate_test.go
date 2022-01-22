c6d1d8302b4605d168c77954d8e90aefad9a6e98
DimaKoz <kozhevin.dima@gmail.com>
2022-01-22 12:38:53 +0300

    testing for errors

--- aggregate_test.go
+++ aggregate_test.go
@@ -1,6 +1,9 @@
 package moexiss
 
-import "testing"
+import (
+	"github.com/buger/jsonparser"
+	"testing"
+)
 
 func TestAggregatesGetUrl(t *testing.T) {
 	var income *AggregateRequestOptions = nil
@@ -33,5 +36,58 @@ func TestParseAggregate(t *testing.T) {
 	}
 	if got, expected := aggregate, expectedStruct; got != expected {
 		t.Fatalf("Error: expecting: \n %v \ngot:\n %v \ninstead", expected, got)
+	}
+}
+
+func TestParseAggregateErrCases(t *testing.T) {
+	type Case struct {
+		incomeJson string
+		expected   error
+	}
+	cases := []Case{
+		// no market_name
+		{`{"1": "shares", "market_title": "Рынок акций", "engine": "stock", "tradedate": "2022-01-19", "secid": "SBERP", "value": 9833418828.24, "volume": 42115503, "numtrades": 144467, "updated_at": "2022-01-20 09:00:14"}`,
+			jsonparser.KeyPathNotFoundError,
+		},
+		// no market_title
+		{`{"market_name": "shares", "1": "Рынок акций", "engine": "stock", "tradedate": "2022-01-19", "secid": "SBERP", "value": 9833418828.24, "volume": 42115503, "numtrades": 144467, "updated_at": "2022-01-20 09:00:14"}`,
+			jsonparser.KeyPathNotFoundError,
+		},
+		// no engine
+		{`{"market_name": "shares", "market_title": "Рынок акций", "engine1": "stock", "tradedate": "2022-01-19", "secid": "SBERP", "value": 9833418828.24, "volume": 42115503, "numtrades": 144467, "updated_at": "2022-01-20 09:00:14"}`,
+			jsonparser.KeyPathNotFoundError,
+		},
+		// no tradedate
+		{`{"market_name": "shares", "market_title": "Рынок акций", "engine": "stock", "tradedate1": "2022-01-19", "secid": "SBERP", "value": 9833418828.24, "volume": 42115503, "numtrades": 144467, "updated_at": "2022-01-20 09:00:14"}`,
+			jsonparser.KeyPathNotFoundError,
+		},
+		// no secid
+		{`{"market_name": "shares", "market_title": "Рынок акций", "engine": "stock", "tradedate": "2022-01-19", "secid1": "SBERP", "value": 9833418828.24, "volume": 42115503, "numtrades": 144467, "updated_at": "2022-01-20 09:00:14"}`,
+			jsonparser.KeyPathNotFoundError,
+		},
+		// no value
+		{`{"market_name": "shares", "market_title": "Рынок акций", "engine": "stock", "tradedate": "2022-01-19", "secid": "SBERP", "value1": 9833418828.24, "volume": 42115503, "numtrades": 144467, "updated_at": "2022-01-20 09:00:14"}`,
+			jsonparser.KeyPathNotFoundError,
+		},
+		// no volume
+		{`{"market_name": "shares", "market_title": "Рынок акций", "engine": "stock", "tradedate": "2022-01-19", "secid": "SBERP", "value": 9833418828.24, "volume1": 42115503, "numtrades": 144467, "updated_at": "2022-01-20 09:00:14"}`,
+			jsonparser.KeyPathNotFoundError,
+		},
+		// no numtrades
+		{`{"market_name": "shares", "market_title": "Рынок акций", "engine": "stock", "tradedate": "2022-01-19", "secid": "SBERP", "value": 9833418828.24, "volume": 42115503, "numtrades1": 144467, "updated_at": "2022-01-20 09:00:14"}`,
+			jsonparser.KeyPathNotFoundError,
+		},
+		// no updated_at
+		{`{"market_name": "shares", "market_title": "Рынок акций", "engine": "stock", "tradedate": "2022-01-19", "secid": "SBERP", "value": 9833418828.24, "volume": 42115503, "numtrades": 144467, "updated_at1": "2022-01-20 09:00:14"}`,
+			jsonparser.KeyPathNotFoundError,
+		},
+	}
+
+	for i, c := range cases {
+		aggregate := Aggregate{}
+		if got, expected := parseAggregate([]byte(c.incomeJson), &aggregate), c.expected; got != expected {
+			t.Fatalf("Error: expecting error: \n %v \ngot:\n %v \ninstead in %d case", expected, got, i)
+		}
+
 	}
 }
