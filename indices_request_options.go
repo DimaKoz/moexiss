c552b9e45aeed1490bfae1c6af45ad0de28d9b6b
DimaKoz <kozhevin.dima@gmail.com>
2022-01-26 20:32:41 +0300

    request options of indices

---
+++ indices_request_options.go
@@ -0,0 +1,50 @@
+package moexiss
+
+import "net/url"
+
+//IndicesRequestOptions contains options which can be used as arguments
+//for building requests to get a list of indices that include the security.
+//MoEx ISS API docs: https://iss.moex.com/iss/reference/160
+type IndicesRequestOptions struct {
+	lang Language  // `lang` query parameter in url.URL
+}
+
+//IndicesRequestOptionsBuilder represents a builder of IndicesRequestOptions struct
+type IndicesRequestOptionsBuilder struct {
+	options *IndicesRequestOptions
+}
+
+//NewIndicesReqOptionsBuilder is a constructor of IndicesRequestOptionsBuilder
+func NewIndicesReqOptionsBuilder() *IndicesRequestOptionsBuilder {
+	return &IndicesRequestOptionsBuilder{options: &IndicesRequestOptions{}}
+}
+
+//Build builds IndicesRequestOptions from IndicesRequestOptionsBuilder
+func (b *IndicesRequestOptionsBuilder) Build() *IndicesRequestOptions {
+	return b.options
+}
+
+//Lang sets 'lang' parameter to a request
+func (b *IndicesRequestOptionsBuilder) Lang(lang Language) *IndicesRequestOptionsBuilder {
+	b.options.lang = lang
+	return b
+}
+
+//addIndicesRequestOptions sets parameters into *url.URL
+//from IndicesRequestOptions struct and returns it back
+func addIndicesRequestOptions(url *url.URL, options *IndicesRequestOptions) *url.URL {
+	q := url.Query()
+	q.Set("iss.meta", "off")
+	q.Set("iss.json", "extended")
+	if options == nil {
+		url.RawQuery = q.Encode()
+		return url
+	}
+
+	if options.lang != LangUndefined {
+		q.Set("lang", options.lang.String())
+	}
+
+	url.RawQuery = q.Encode()
+	return url
+}
---
+++ indices_request_options_test.go
@@ -0,0 +1,61 @@
+package moexiss
+
+import (
+	"testing"
+)
+
+func TestIndicesReqOptionsBuilder_Build(t *testing.T) {
+	expectStruct := IndicesRequestOptions{}
+	bld := NewIndicesReqOptionsBuilder()
+
+	if got, expected := *bld.Build(), expectStruct; got != expected {
+		t.Fatalf("Error: expecting `%v` AggregateRequestOptions \ngot `%v` AggregateRequestOptions \ninstead", expected, got)
+	}
+}
+
+func TestIndicesReqOptionsBuilder_Lang(t *testing.T) {
+	expectStruct := IndicesRequestOptions{lang: LangEn}
+	bld := NewIndicesReqOptionsBuilder()
+	bld.Lang(LangEn)
+	if got, expected := *bld.Build(), expectStruct; got != expected {
+		t.Fatalf("Error: expecting `%v` \ngot `%v` \ninstead", expected, got)
+	}
+}
+
+func TestNewIndicesRequestOptions(t *testing.T) {
+	expectStruct := IndicesRequestOptions{
+		lang: LangRu,
+	}
+	bld := NewIndicesReqOptionsBuilder()
+	bld.Lang(LangRu)
+	if got, expected := *bld.Build(), expectStruct; got != expected {
+		t.Fatalf("Error: expecting `%v` \ngot `%v` \ninstead", expected, got)
+	}
+
+}
+
+func TestAddIndicesRequestOptionsNilOptions(t *testing.T) {
+	var income *IndicesRequestOptions = nil
+	c := NewClient(nil)
+	url, _ := c.BaseURL.Parse("test.json")
+	gotUrl := addIndicesRequestOptions(url, income)
+
+	expected := `https://iss.moex.com/iss/test.json?iss.json=extended&iss.meta=off`
+	if got := gotUrl.String(); got != expected {
+		t.Fatalf("Error: expecting url :\n`%s` \ngot \n`%s` \ninstead", expected, got)
+	}
+}
+
+func TestAddIndicesRequestOptions(t *testing.T) {
+	var incomeOptions = NewIndicesReqOptionsBuilder().
+		Lang(LangEn).
+		Build()
+	c := NewClient(nil)
+	url, _ := c.BaseURL.Parse("test.json")
+	gotUrl := addIndicesRequestOptions(url, incomeOptions)
+
+	expected := `https://iss.moex.com/iss/test.json?iss.json=extended&iss.meta=off&lang=en`
+	if got := gotUrl.String(); got != expected {
+		t.Fatalf("Error: expecting url :\n`%s` \ngot \n`%s` \ninstead", expected, got)
+	}
+}
