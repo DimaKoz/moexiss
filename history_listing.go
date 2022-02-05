2df93b55cf147bbc62ccde5130326bfc810800d4
DimaKoz <kozhevin.dima@gmail.com>
2022-02-05 08:29:17 +0300

    parsing a listing item

--- history_listing.go
+++ history_listing.go
@@ -1,6 +1,8 @@
 package moexiss
 
-import "path"
+import (
+	"path"
+)
 
 //Listing struct represents listing of the security
 type Listing struct {
@@ -8,7 +10,7 @@ type Listing struct {
 	ShortName string // "SHORTNAME"
 	FullName  string // "NAME"
 	BoardId   string // "BOARDID"
-	Decimals  int32  // "decimals"
+	Decimals  int64  // "decimals"
 	From      string // "history_from"
 	Till      string // "history_till"
 }
@@ -40,4 +42,52 @@ func (i *HistoryListingService) getUrlListing(engine EngineName, market string, 
 	url.Path = path.Join(url.Path, engine.String(), "markets", market, "listing.json")
 	gotUrl := addHistoryListingRequestOptions(url, opt)
 	return gotUrl.String(), nil
+}
+
+func parseListingItem(data []byte, l *Listing) (err error) {
+
+	ticker, err := parseStringWithDefaultValueByKey(data, listingKeyId, "")
+	if err != nil {
+		return
+	}
+
+	shortName, err := parseStringWithDefaultValueByKey(data, listingKeyShortName, "")
+	if err != nil {
+		return
+	}
+
+	name, err := parseStringWithDefaultValueByKey(data, listingKeyName, "")
+	if err != nil {
+		return
+	}
+
+	boardId, err := parseStringWithDefaultValueByKey(data, listingKeyBoardId, "")
+	if err != nil {
+		return
+	}
+
+	decimal, err := parseIntWithDefaultValue(data, listingKeyDecimals)
+	if err != nil {
+		return
+	}
+
+	from, err := parseStringWithDefaultValueByKey(data, listingKeyFrom, "")
+	if err != nil {
+		return
+	}
+
+	till, err := parseStringWithDefaultValueByKey(data, listingKeyTill, "")
+	if err != nil {
+		return
+	}
+
+	l.Ticker = ticker
+	l.ShortName = shortName
+	l.FullName = name
+	l.BoardId = boardId
+	l.Decimals = decimal
+	l.From = from
+	l.Till = till
+
+	return
 }
--- history_listing_test.go
+++ history_listing_test.go
@@ -1,6 +1,9 @@
 package moexiss
 
-import "testing"
+import (
+	"github.com/buger/jsonparser"
+	"testing"
+)
 
 func TestHistoryListingGetUrl(t *testing.T) {
 	c := NewClient(nil)
@@ -10,5 +13,58 @@ func TestHistoryListingGetUrl(t *testing.T) {
 	}
 	if got, expected := gotUrl, `https://iss.moex.com/iss/history/engines/stock/markets/shares/listing.json?iss.json=extended&iss.meta=off`; got != expected {
 		t.Fatalf("Error: expecting url :\n`%s` \ngot \n`%s` \ninstead", expected, got)
+	}
+}
+
+func TestParseListingItem(t *testing.T) {
+	expectedStruct := Listing{
+		Ticker:    "BSPB",
+		ShortName: "БСП ао",
+		FullName:  "ПАО \"Банк \"Санкт-Петербург\" ао",
+		BoardId:   "TQBR",
+		Decimals:  2,
+		From:      "2014-06-09",
+		Till:      "2022-02-04",
+	}
+	var incomeJson = `
+      {"SECID": "BSPB", "SHORTNAME": "БСП ао", "NAME": "ПАО \"Банк \"Санкт-Петербург\" ао", "BOARDID": "TQBR", "decimals": 2, "history_from": "2014-06-09", "history_till": "2022-02-04"}
+`
+	listingItem := Listing{}
+	err := parseListingItem([]byte(incomeJson), &listingItem)
+	if err != nil {
+		t.Fatalf("Error: expecting <nil> error: \ngot %v \ninstead", err)
+	}
+	if got, expected := listingItem, expectedStruct; got != expected {
+		t.Fatalf("Error: expecting: \n %v \ngot:\n %v \ninstead", expected, got)
+	}
+}
+
+func TestParseListingItemErrCases(t *testing.T) {
+	type Case struct {
+		incomeJson string
+	}
+	cases := []Case{
+		// no SECID
+		{`{"SECID1": "BSPB", "SHORTNAME": "БСП ао", "NAME": "ПАО \"Банк \"Санкт-Петербург\" ао", "BOARDID": "TQBR", "decimals": 2, "history_from": "2014-06-09", "history_till": "2022-02-04"}`},
+		// no SHORTNAME
+		{`{"SECID": "BSPB", "SHORTNAME1": "БСП ао", "NAME": "ПАО \"Банк \"Санкт-Петербург\" ао", "BOARDID": "TQBR", "decimals": 2, "history_from": "2014-06-09", "history_till": "2022-02-04"}`},
+		// no NAME
+		{`{"SECID": "BSPB", "SHORTNAME": "БСП ао", "NAME1": "ПАО \"Банк \"Санкт-Петербург\" ао", "BOARDID": "TQBR", "decimals": 2, "history_from": "2014-06-09", "history_till": "2022-02-04"}`},
+		// no BOARDID
+		{`{"SECID": "BSPB", "SHORTNAME": "БСП ао", "NAME": "ПАО \"Банк \"Санкт-Петербург\" ао", "BOARDID1": "TQBR", "decimals": 2, "history_from": "2014-06-09", "history_till": "2022-02-04"}`},
+		// no decimals
+		{`{"SECID": "BSPB", "SHORTNAME": "БСП ао", "NAME": "ПАО \"Банк \"Санкт-Петербург\" ао", "BOARDID": "TQBR", "decimals1": 2, "history_from": "2014-06-09", "history_till": "2022-02-04"}`},
+		// no history_from
+		{`{"SECID": "BSPB", "SHORTNAME": "БСП ао", "NAME": "ПАО \"Банк \"Санкт-Петербург\" ао", "BOARDID": "TQBR", "decimals": 2, "history_from1": "2014-06-09", "history_till": "2022-02-04"}`},
+		// no history_till
+		{`{"SECID": "BSPB", "SHORTNAME": "БСП ао", "NAME": "ПАО \"Банк \"Санкт-Петербург\" ао", "BOARDID": "TQBR", "decimals": 2, "history_from": "2014-06-09", "history_till1": "2022-02-04"}`},
+	}
+
+	for i, c := range cases {
+		listing := Listing{}
+		if got, expected := parseListingItem([]byte(c.incomeJson), &listing), jsonparser.KeyPathNotFoundError; got != expected {
+			t.Fatalf("Error: expecting error: \n %v \ngot:\n %v \ninstead in %d case", expected, got, i)
+		}
+
 	}
 }
