ebc4b35d12516ac00aac87b533dd8e2084bc05a9
DimaKoz <kozhevin.dima@gmail.com>
2022-02-17 20:45:21 +0300

    'SecStat' structure added

--- stat.go
+++ stat.go
@@ -9,6 +9,33 @@ const (
 	statsPartsUrl = "secstats.json"
 )
 
+// SecStat struct represents intermediate day summary
+// by the security by markets
+type SecStat struct {
+	Ticker           string         // "SECID"
+	BoardId          string         // "BOARDID"
+	TrSession        TradingSession // "TRADINGSESSION"
+	Time             string         // "TIME"
+	PriceMinusPrevPr float64        // "PRICEMINUSPREVWAPRICE"
+	VolToday         int64          // "VOLTODAY"
+	ValToday         int64          // "VALTODAY"
+	HighBid          float64        // "HIGHBID"
+	LowOffer         float64        // "LOWOFFER"
+	LastOffer        float64        // "LASTOFFER"
+	LastBid          float64        // "LASTBID"
+	Open             float64        // "OPEN"
+	Low              float64        // "LOW"
+	High             float64        // "HIGH"
+	Last             float64        // "LAST"
+	LClosePrice      float64        // "LCLOSEPRICE"
+	NumTrades        int64          // "NUMTRADES"
+	WaPrice          float64        // "WAPRICE"
+	AdmittedQuote    float64        // "ADMITTEDQUOTE"
+	MarketPrice      float64        // "MARKETPRICE2"
+	LCurrentPrice    float64        // "LCURRENTPRICE"
+	ClosingAucPrice  float64        // "CLOSINGAUCTIONPRICE"
+}
+
 // StatsService gets intermediate day summary
 // from the MoEx ISS API.
 //
